757c9125d2d1f19e2f03e8631168557540a0771e
llattanzi <lauren71442@gmail.com>
2022-04-21 21:47:10 -0400

    Add remaining prompts for all employees and build an employee list of objects

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 // create an empty array to store all generated employees
-const employees = [];
+const employeeList = [];
 
 const promptMenu = () => {
     inquirer.prompt({
@@ -16,15 +16,36 @@ const promptMenu = () => {
         })
         .then(({ role }) => {
             if (role === 'Engineer') {
-                addEngineer();
+                // create Engineer obj and run user prompts
+                employee = new Engineer().getBasics().getGithub();
+                // add new employee to list
+                employeeList.push(employee);
+                // go back to menu
+                return promptMenu();
             }
             else if (role === 'Intern') {
-                addIntern();
+                // create Intern obj and run user prompts
+                employee = new Intern().getBasics().getSchool();
+                // add new employee to list
+                employeeList.push(employee);
+                // go back to menu
+                return promptMenu();
             }
             else {
                 console.log("Your team is complete.")
+                console.log(employeeList)
+                return employeeList;
             }
         })
 }
 
-console.log(promptManager())
+getBasics = function() {
+    this.getName().getId().getEmail().getRole();
+}
+
+// prompt for manager info first. this only happens once per team
+let employee = new Manager().getBasics().getOfficeNumber();
+// push manager to employee array
+employeeList.push(employee);
+// go to menu (prompt user to add engineers or interns)
+promptMenu();
